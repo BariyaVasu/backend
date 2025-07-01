c9bab61edc51ef92061adaa44aea361293b2e6a7
Vasu-0846 <bariyavasu46@gmail.com>
2025-07-01 10:44:17 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,94 @@
+import express from "express";
+import cors from "cors"
+const app = express();
+const port = 9000;
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
+
+
+//old way to add function
+// function abc (){}
+// app.get("/", abc)
+
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
+// app.get("/product", (req, res) => {
+//     res.send([
+//         {
+//             "product": 1,
+//             "category": "Smart Phone",
+//             "product name": "IPhone"
+
+//         },
+//         {
+//             "product": 2,
+//             "category": "TV",
+//             "product name": "Samsung LED Tv"
+
+//         },
+//         {
+//             "product": 3,
+//             "category": "Laptop",
+//             "product name": "Asus Tuf Gaming Laptop"
+
+//         },
+//         {
+//             "product": 4,
+//             "category": "Accessories",
+//             "product name": "Gaming Keyboard"
+
+//         },
+//         {
+//             "product": 5,
+//             "category": "Accessories",
+//             "product name": "Gaming Mouse"
+
+//         },
+//         {
+//             "product": 6,
+//             "category": "Laptop",
+//             "product name": "Lenova ideapad Gaming Laptop"
+
+//         },
+//         {
+//             "product": 7,
+//             "category": "Smart Phone",
+//             "product name": "Samsung Galaxy S24"
+
+//         },
+//         {
+//             "product": 8,
+// "category": "Tablet",
+//             "product name": "IPad 10 Pro"
+
+//         },
+//         {
+//             "product": 9,
+//             "category": "Accessories",
+//             "product name": "Gaming HeadPhone"
+
+//         },
+//         {
+//             "product": 10,
+//             "category": "Laptop",
+//             "product name": "Samsung"
+
+//         },
+//     ]);
+// })
+
+app.get("/:color", (req, res) => {
+    // console.log(req.params.id);
+    res.send(req.params.color);
+})
+
+app.listen(port, () => {
+    console.log(`Server is Running on Port ${port}`);
+})
